574dcd6cab9eddd69c21c64b4365611716889f47
Alexander Melnikov <Msan90546@mail.ru>
2025-11-09 17:36:31 +0300

    First task solved

---
+++ 2.1/PrintText.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n";
+    std::cout << "  - is a better C\n  - supports data abstraction\n  - supports object-oriented programming\n";
+    std::cout << "  - supports generic programming.";
+}
